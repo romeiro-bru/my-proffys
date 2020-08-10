b7cb04c3b3d2bd289548947537c66cb6b57efe10
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-10 01:46:01 -0300

    Add post method

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ const server = express()
 const  {
     pageLanding,
     pageStudy,
-    pageTeach
+    pageTeach,
+    saveClasses
 } = require('./pages')
 
 const nunjucks = require('nunjucks')
@@ -18,12 +19,15 @@ nunjucks.configure('src/views', {
 })
 
 // INICIO E CONFIG DO SERVIDOR
-
+server
+// receber dados do body
+.use(express.urlencoded({ extended: true }))
 // configure static files (css, scripts, images)
-server.use(express.static("public"))
+.use(express.static("public"))
 // app routes
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/teach", pageTeach)
+.post("/save-classes", saveClasses)
 // START SERVER
 .listen(5000)
