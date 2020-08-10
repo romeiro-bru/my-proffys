d4c3253dfe0f86ca41ffdd6ce150a71122a8473e
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-10 01:45:23 -0300

    Converter horas em min

--- src/pages.js
+++ src/pages.js
@@ -37,6 +37,10 @@ async function pageStudy (req, res) {
         const db = await Database
         const proffys = await db.all(query)
 
+        proffys.map( (proffy) => {
+            proffy.subject = getSubject(proffy.subject)
+        })
+
         return res.render('study.html', { proffys, subjects, filters, weekdays })
         
     } catch (error) {
@@ -46,21 +50,56 @@ async function pageStudy (req, res) {
 }
 
 function pageTeach (req, res) {
-    const data = req.query
+    return res.render("teach.html", { subjects, weekdays })
+}
+
+async function saveClasses(req, res) {
+    const createProffy = require('./database/createProffy')
+
+    const proffyValue = {
+        name: req.body.name,
+        avatar: req.body.avatar,
+        whatsapp: req.body.whatsapp,
+        bio: req.body.bio
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost
+    }
+
+    const classScheduleValues = req.body.weekday.map((weekday, index) => {
+        return {
+            weekday, 
+            time_from: convertHoursToMinutes(req.body.time_from[index]),
+            time_to: convertHoursToMinutes(req.body.time_to[index])
+        }
+    })
+
+    try {        
+        const db = await Database
+        await createProffy(db, { proffyValue, classValue, classScheduleValues })
+        
+        let queryString = "?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+
+        return res.redirect("/study" + queryString)
+    } catch (error) {
+        console.log(error)
+    }
     // add data into proffys object
     // if we have data, add the data
     if (Object.keys(data).length > 0) {
         data.subject = getSubject(data.subject)
         // console.log(data)
         proffys.push(data)
-        return res.redirect("/study")
     }
-    // if doesn't then do not add and show teach page again
-    return res.render("teach.html", { subjects, weekdays })
 }
 
 module.exports = {
     pageLanding,
     pageStudy,
-    pageTeach
+    pageTeach,
+    saveClasses
 }
